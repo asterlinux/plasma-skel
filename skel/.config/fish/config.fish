4053691b69c6f097bbb1b236accfa78d162ad4ef
asterlinux <sahan.user@gmail.com>
2023-01-26 11:11:50 +0530

    new alias

--- skel/.config/fish/config.fish
+++ skel/.config/fish/config.fish
@@ -166,40 +166,26 @@ function take --argument number
     head -$number
 end
 
-# Function for org-agenda
-function org-search -d "send a search string to org-mode"
-    set -l output (/usr/bin/emacsclient -a "" -e "(message \"%s\" (mapconcat #'substring-no-properties \
-        (mapcar #'org-link-display-format \
-        (org-ql-query \
-        :select #'org-get-heading \
-        :from  (org-agenda-files) \
-        :where (org-ql--query-string-to-sexp \"$argv\"))) \
-        \"
-    \"))")
-    printf $output
-end
-
 ### END OF FUNCTIONS ###
 
 
 ### ALIASES ###
-# \x1b[2J   <- clears tty
-# \x1b[1;1H <- goes to (1, 1) (start)
 alias clear='echo -en "\x1b[2J\x1b[1;1H" ; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo'
 
 
 # Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
-alias l.='exa -a | egrep "^\."'
+alias ls='exa --color=always --group-directories-first'
+alias la='exa -la --color=always --octal-permissions --no-permissions --group-directories-first'
+alias l='exa -l --color=always --group-directories-first'
 
 # confirm before overwriting something
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
 
-# system info programme
+# change cat to bat
+
+alias cat="bat"
+
 source (/usr/bin/starship init fish --print-full-init | psub)
 pfetch
