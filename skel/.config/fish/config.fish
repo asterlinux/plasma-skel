d3ae8fa6bfdae66d7e4372e907bcf0a35fc372d0
asterlinux <sahan.user@gmail.com>
2023-01-26 22:07:07 +0530

    fish config

--- skel/.config/fish/config.fish
+++ skel/.config/fish/config.fish
@@ -13,6 +13,41 @@ function fish_user_key_bindings
   fish_vi_key_bindings
 end
 
+# # ex = EXtractor for all kinds of archives
+# # usage: ex -a file.tar.gz
+function ex -a file
+    if test -f "$file"
+        switch "$file"
+            case "*.tar.bz2"
+                tar xjf $file
+            case "*.tar.gz"
+                tar xzf $file
+            case "*.bz2"
+                bunzip2 $file
+            case "*.rar"
+                unrar x $file
+            case "*.gz"
+                gunzip $file
+            case "*.tar"
+                tar xf $file
+            case "*.tbz2"
+                tar xjf $file
+            case "*.tgz"
+                tar xzf $file
+            case "*.zip"
+                unzip $file
+            case "*.Z"
+                uncompress $file
+            case "*.7z"
+                7z x $file
+            case "*"
+                echo "'$file' cannot be extracted via ex -a <file>"
+        end
+    else
+        echo "'$file' is not a valid file"
+    end
+end
+
 ### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
 set fish_color_normal brcyan
 set fish_color_autosuggestion '#7d7d7d'
@@ -34,6 +69,15 @@ alias rm='rm -i'
 
 # change cat to bat
 alias cat="bat"
+
+# typo fixes
+alias cd..='cd ..'
+alias pdw='pwd'
+
+# colored grep out 
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
 
 source (/usr/bin/starship init fish --print-full-init | psub)
 pfetch
